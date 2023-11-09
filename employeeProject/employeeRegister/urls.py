8c5b92b9f78709f1312bf1935dda95208e0bb09f
Haruto Mori <baystarsman@outlook.jp>
2023-11-09 17:08:05 +0900

    give name to each url path

--- employeeProject/employeeRegister/urls.py
+++ employeeProject/employeeRegister/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form),
-    path('list/', views.employee_list),
+    path('', views.employee_form, name='form'),
+    path('list/', views.employee_list, name='list'),
 ]
