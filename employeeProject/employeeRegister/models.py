a177e943ef779ac71c2579c6017d7387420cf55f
Haruto Mori <baystarsman@outlook.jp>
2023-11-09 13:38:27 +0900

    show the concrete name of data in the admin panel

--- employeeProject/employeeRegister/models.py
+++ employeeProject/employeeRegister/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Position(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
@@ -11,3 +14,5 @@ class Employee(models.Model):
     mobile = models.CharField(max_length=20)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.fullname
