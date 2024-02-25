188ffc313a35f3e958679750b90c13191ff93ee9
Haruto Mori <baystarsman@outlook.jp>
2024-02-25 18:53:39 +0900

    feat: add view functions for crud

--- todo/blueprints/todo.py
+++ todo/blueprints/todo.py
@@ -13,3 +13,19 @@ bp = Blueprint('todo', __name__, url_prefix='/todo')
 @bp.route('/')
 def todo():
     return render_template('todo.html')
+
+
+@bp.route('/create')
+def create():
+    pass
+
+
+@bp.route('/update')
+def update():
+    pass
+
+
+@bp.route('/delete')
+def delete():
+    pass
+
