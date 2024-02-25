52162e9e70ee534b71b4b745e2a8bf2aa5d90a61
Haruto Mori <baystarsman@outlook.jp>
2024-02-25 17:53:24 +0900

    feat: set init-db command

--- todo/db.py
+++ todo/db.py
@@ -28,3 +28,8 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read.decode('utf8'))
 
+
+@click.command('init-db')
+def init_db_command():
+    init_db()
+    click.echo("Initialized the database")
